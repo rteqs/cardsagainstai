88407316201af53f5799792c0e9d44ff6371b18a
RyanTeoh <ryanteoh01@gmail.com>
2021-03-07 15:09:46 -0800

    fix(package.json): adding missing dependencies
    adding eslint-plugin-prettier, eslint-config-prettier, eslint-plugin-jest

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,6 +12,12 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', 'prettier'],
-  rules: { 'prettier/prettier': 'error' },
+  rules: {
+    'prettier/prettier': 'error',
+    // suppress errors for missing 'import React' in files
+    'react/react-in-jsx-scope': 'off',
+    // allow jsx syntax in js files (for next.js project)
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
+  },
   root: true,
 };
