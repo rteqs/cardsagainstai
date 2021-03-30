7c63806f5edd4df6f4795575e7f2246203da691c
yousefh409 <yousefh409@gmail.com>
2021-03-30 11:50:39 -0700

    some quick changes

--- client/src/components/BlackCardTemplate.js
+++ client/src/components/BlackCardTemplate.js
@@ -1,31 +1,45 @@
+
 function BlackCardTemplate(args)
 {
 	const outerDivStyle = {
-    float: 'left',
+		float: 'left',
 		margin: '20px',
   	height: '250px',
   	width: '200px',
   	backgroundColor:"#050505",
-     borderRadius: "10px"
-  	}
-	const innerDivStyle = {
-    marginRight: '10px',
-    marginLeft: '10px',
-      width: '176px',
-      color: 'white',
-    }
-  console.log(args)
-  return (
-      <div className="Card" style={outerDivStyle}>
-        <div style = {innerDivStyle}>
-            <header className="A card">
-              <p>
-                {args.text}
-              </p>
-            </header>
-          </div>
-      </div>
-    );
+		borderRadius: "10px",
+		position: 'relative',
+    color: "#fafafa"
+  }
+  	const innerDivStyle = {
+		marginRight: '10px',
+		marginLeft: '10px',
+  	width: '176px',
+  }
+
+	const blackCardText = {
+		fontWeight: "bold",
+		fontSize: "xlarge",
+		fontFamily: 'Helvetica'
+	}
+	console.log(args)
+	return (
+    	<div className="gameCards" style={outerDivStyle}>
+    		<div style = {innerDivStyle}>
+	      		<header className="A card">
+	        		<p>
+	         			{args.text}
+	        		</p>
+	      		</header>
+				<div style={{position: "absolute", bottom: 0, left: 0, margin: "13px"}}>
+					<div style={blackCardText}>Cards</div>
+					<div style={blackCardText}>Against</div>
+					<div style={blackCardText}>AI</div>
+				</div>
+
+	      	</div>
+    	</div>
+  	);
 }
 
 export default BlackCardTemplate;
--- client/src/components/WhiteCardTemplate.js
+++ client/src/components/WhiteCardTemplate.js
@@ -1,3 +1,4 @@
+import "../styles/gameScreen.css";
 
 function WhiteCardTemplate(args)
 {
@@ -7,22 +8,36 @@ function WhiteCardTemplate(args)
   		height: '250px',
   		width: '200px',
   		backgroundColor:"#fafafa",
-		borderRadius: "10px"
+		borderRadius: "10px",
+		position: 'relative',
+		transition: "rotation .1s ease-in-out",
   	}
   	const innerDivStyle = {
 		marginRight: '10px',
 		marginLeft: '10px',
   		width: '176px',
   	}
+
+	const whiteCardText = {
+		fontWeight: "bold",
+		fontSize: "xlarge",
+		fontFamily: 'Helvetica'
+	}
 	console.log(args)
 	return (
-    	<div className="gameCards" style={outerDivStyle}>
+    	<div className="gameWhiteCard" style={outerDivStyle}>
     		<div style = {innerDivStyle}>
 	      		<header className="A card">
 	        		<p>
 	         			{args.text}
 	        		</p>
 	      		</header>
+				<div style={{position: "absolute", bottom: 0, left: 0, margin: "13px"}}>
+					<div style={whiteCardText}>Cards</div>
+					<div style={whiteCardText}>Against</div>
+					<div style={whiteCardText}>AI</div>
+				</div>
+
 	      	</div>
     	</div>
   	);
