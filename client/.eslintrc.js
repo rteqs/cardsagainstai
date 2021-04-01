7cc8079fca49448e3d514729992528fc90cd425a
RyanTeoh <ryanteoh01@gmail.com>
2021-03-31 20:05:19 -0700

    fix: adding eslintrc settings

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,14 +14,19 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 0,
-    //'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    // 'prettier/prettier': ['error', { endOfLine: 'auto' }],
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // should add ".ts" if typescript project
   },
-  // "rules": {     
-  //   "prettier/prettier": ["error", { "endOfLine": "auto" }]   
-  // },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   root: true,
 };
+
