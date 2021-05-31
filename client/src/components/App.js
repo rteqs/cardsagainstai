6dc2f4f0a7f545cb68e2426d07e0a388018143af
Austin Cheng <austinbicheng@gmail.com>
2021-05-30 18:54:08 -0700

    test auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,9 @@ import WinScreen from '../pages/WinScreen';
 import Authenticate from '../pages/Authenticate';
 // import Api from '../Api';
 
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="1079919932041-6lp67a32pntnsej5p56icgtlrsegs1co.apps.googleusercontent.com">
+
 export default function App() {
   const dispatch = useDispatch();
 
@@ -56,6 +59,8 @@ export default function App() {
               >
                 Go to The Lobby
               </button>
+              <div class="g-signin2" data-onsuccess="onSignIn"></div>
+              <a href="#" onclick="signOut();">Sign out</a>
             </div>
           </div>
         )}
@@ -63,3 +68,19 @@ export default function App() {
     </Switch>
   );
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
+
